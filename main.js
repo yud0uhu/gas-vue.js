78b3fc330ef7b198012f4c482e0d7cbf9d315f9f
yud0uhu <60646787+yud0uhu@users.noreply.github.com>
2020-10-25 20:22:11 +0900

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-function doGet() {
-  var htmlOutput = HtmlService.createTemplateFromFile("index").evaluate();
-  htmlOutput
-    .setTitle('GAS+Vue.js')
-    .addMetaTag('viewport', 'width=device-width, initial-scale=1')
-  return htmlOutput;
-}
